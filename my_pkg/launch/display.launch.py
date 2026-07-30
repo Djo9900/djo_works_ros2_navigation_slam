1e4fa76d7746d5f862085406e345d2a141d23c7c
Djo9900 <siradjosy1@gmail.com>
2026-07-31 00:28:54 +0200

    config de ros2_control

--- my_pkg/launch/display.launch.py
+++ my_pkg/launch/display.launch.py
@@ -1,9 +1,11 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription,DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution, TextSubstitution
+from launch.substitutions import PathJoinSubstitution, TextSubstitution,PythonExpression
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition, UnlessCondition
 import os
 import xacro
 import launch
@@ -17,6 +19,12 @@ def generate_launch_description():
 
 
     pkg_gazebo = get_package_share_directory('ros_gz_sim')
+    
+    # Déclaration de la variable use_sim_time
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    use_ros2_control = LaunchConfiguration('use_ros2_control', default='true')
+    
+    # use_rviz = LaunchConfiguration("use_rviz").perform(context)
 
     gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -30,20 +38,34 @@ def generate_launch_description():
 
     # === Retourne la description du lancement ===
     return LaunchDescription([
+        
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='true',
+            description='Use simulation (Gazebo) clock if true'
+        ),
+        
         # Publie la description du robot dans /robot_description
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
             name='robot_state_publisher',
             output='screen',
-            parameters=[{'robot_description': robot_desc}]
+            parameters=[{'robot_description': robot_desc,
+                         'use_sim_time': use_sim_time}]
         ),
 
         # Interface graphique pour les articulations (utile pour tests)
         Node(
-            package='joint_state_publisher_gui',
-            executable='joint_state_publisher_gui',
-            name='joint_state_publisher_gui'
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+            parameters=[
+            {
+                "robot_description": robot_desc,
+                "use_sim_time": use_sim_time
+            }
+        ],
         ),
 
         # Lancement de RViz
@@ -51,18 +73,38 @@ def generate_launch_description():
             package='rviz2',
             executable='rviz2',
             name='rviz2',
-            output='screen'
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time}]
+            
+            
         ),
         
+        Node(
+            package="controller_manager",
+            executable="spawner",
+            arguments=["joint_broad", "--controller-manager-timeout", "60"],
+            #condition=IfCondition(PythonExpression(['"', use_ros2_control, '" == "true"']))
+            
+        ),
+        
+        
+        Node(
+            package="controller_manager",
+            executable="spawner",
+            arguments=["diff_drive_controller", "--controller-manager-timeout", "60"],
+            #condition=IfCondition(PythonExpression(['"', use_ros2_control, '" == "true"'])),
+        ),
         
          ####" obot_localization_node  pour la fussion des capteur 
-        Node(
-            package='robot_localization',
-            executable='ekf_node',
-            name='ekf_node',
-            output='screen',
-            parameters=[os.path.join(pkg_path, 'config/ekf.yaml')]
-        ),
+        # Node(
+        #     package='robot_localization',
+        #     executable='ekf_node',
+        #     name='ekf_node',
+        #     output='screen',
+        #     parameters=[
+        #                     os.path.join(pkg_path, 'configuration', 'ekf.yaml'),
+        #                     {'use_sim_time': use_sim_time}]
+        # ),
 
         # Pont entre ROS 2 et Gazebo (communication topics)
         Node(
@@ -71,6 +113,7 @@ def generate_launch_description():
             parameters=[{
                 'config_file': os.path.join(pkg_path, 'configuration', 'bridge.yaml'),
                 'qos_overrides./tf_static.publisher.durability': 'transient_local',
+                'use_sim_time': use_sim_time
             }],
             output='screen'
         ),
@@ -95,7 +138,4 @@ def generate_launch_description():
 
         # Lancement de la simulation Gazebo
         gz_sim,
-        
-        launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='True',
-                                            description='Flag to enable use_sim_time'),
     ])
